4cb92b7a24db9e3091c20c37d60576c1a07600eb
murphymy04 <137734306+murphymy04@users.noreply.github.com>
2024-09-02 10:17:52 -0500

    Terminal commands all work except for help

--- myscripts.js
+++ myscripts.js
@@ -6,23 +6,74 @@ button.addEventListener('click', function() {
     var theme = document.getElementsByTagName('link')[0];
     if (theme.getAttribute('href') == 'plain.css') { 
         theme.setAttribute('href', 'terminal.css'); 
+        localStorage.setItem('theme', 'terminal.css');
     } else { 
+        localStorage.setItem('theme', 'plain.css');
         theme.setAttribute('href', 'plain.css'); 
     } 
 });
 
-const input = document.getElementById("terminalInput");
+window.addEventListener('load', function() {
+    const currTheme = this.localStorage.getItem('theme');
+    var theme = document.getElementsByTagName('link')[0];
+    theme.setAttribute('href', currTheme);
+});
+
+const length = document.getElementsByClassName('terminalInput').length;
+const input = document.getElementsByClassName('terminalInput')[length - 1];
 input.addEventListener('keypress', function(event) {
     if (event.key == "Enter") {
         const command = input.value;
         handleCommand(command);
     }
-})
+});
 
 function handleCommand(command) {
     switch (command) {
+        case 'help':
+            console.log("help");
+            break;
         case 'ls':
-            console.log("ls");
+            const output = document.createElement('label');
+            const br = document.createElement('br');
+            output.innerText = "portfolio   qualifications   service   gpt"
+            document.getElementById('terminal').insertAdjacentElement('beforeend', br);
+            document.getElementById('terminal').insertAdjacentElement('beforeend', output);
+            break;
+        case 'cd portfolio':
+            document.location.href = "portfolio.html";
+            break;
+        case 'cd resume':
+            document.location.href = "resume.html";
+            break;
+        case 'cd service':
+            document.location.href = "service.html";
+            break;
+        case 'cd gpt':
+            document.location.href = "gpt.html";
+            break;
     }
+    console.log('Command complete');
+    
+    const length = document.getElementsByClassName('terminalInput').length;
+    document.getElementsByClassName('terminalInput')[length - 1].disabled = true;
+
+    const user = document.createElement('label');
+    user.innerText = 'guest@myles-portfolio:~$ ';
+    const terminalInput = document.createElement('input');
+    terminalInput.type = 'text';
+    terminalInput.className = 'terminalInput';
+    user.appendChild(terminalInput);
+    terminalInput.addEventListener('keypress', function(event) {
+        if (event.key == "Enter") {
+            const command = terminalInput.value;
+            handleCommand(command);
+        }
+    });
+
+    const br = document.createElement('br');
+    document.getElementById('terminal').insertAdjacentElement('beforeend', br);
+    document.getElementById('terminal').insertAdjacentElement('beforeend', user);
+
 }
 
